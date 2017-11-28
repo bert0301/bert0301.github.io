d4b527b9b8571c55be58f1c10af1dcf42b710c06
victoria327 <32109860+victoria327@users.noreply.github.com>
2017-11-28 12:57:14 -0500

    Add files via upload

---
+++ midterm-web/footer.php
@@ -0,0 +1,25 @@
+<div class="py-5 bg-primary text-dark">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9">
+          <p class="lead">Contact us to suggest some memes!</p>
+          <form class="form-inline">
+          </form>
+        </div>
+        <div class="col-4 col-md-1 align-self-center">
+          <a href="https://www.facebook.com" target="_blank"><i class="fa fa-fw fa-facebook fa-3x text-dark"></i></a>
+        </div>
+        <div class="col-4 col-md-1 align-self-center">
+          <a href="https://twitter.com" target="_blank"><i class="fa fa-fw fa-twitter fa-3x text-dark"></i></a>
+        </div>
+        <div class="col-4 col-md-1 align-self-center">
+          <a href="https://www.instagram.com" target="_blank"><i class="fa fa-fw fa-instagram fa-3x text-dark"></i></a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 mt-3 text-center">
+          <p>© Copyright 2017 What Does it Meme? &nbsp;- All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  </div>
---
+++ midterm-web/navigation.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+<link rel="stylesheet" href="home.css" type="text/css">
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md bg-primary navbar-light">
+      <div class="container">
+        <a class="navbar-brand" href="#"><b>  What does it meme?</b></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span> </button>
+        <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item px-5">
+              <a class="nav-link active" href="midterm-home.php"> Home</a>
+            </li>
+            <li class="nav-item px-5">
+              <a class="nav-link" href="memes.php"> Memes</a>
+            </li>
+            <li class="nav-item px-5">
+              <a class="nav-link" href="contact.php"> Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </body>
+</html>
